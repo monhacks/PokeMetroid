3cf70aadc16c447b1492a3304f8242ad21521e15
Shock3600 <dalston.levi.jones@gmail.com>
2025-01-10 22:28:04 -0600

    Added more stalkers, intro text, other intro fixes

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,7 +47,7 @@
 #define FLAG_DOUBLE_WILD     0x22 // Unused Flag
 #define FLAG_RECEIVED_QUICK_ATTACK    0x23 // Unused Flag
 #define FLAG_RECEIVED_INFESTATION   0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_PETALBURG_WOODS_VISIITED    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
