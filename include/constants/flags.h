71f03edcc4876c2f1737f5775e4ae90a7e484b0b
Shock3600 <dalston.levi.jones@gmail.com>
2025-01-15 11:35:37 -0600

    Changed start of game flag, see if it stops pokeball from repsawning

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -48,7 +48,7 @@
 #define FLAG_RECEIVED_QUICK_ATTACK    0x23 // Unused Flag
 #define FLAG_RECEIVED_INFESTATION   0x24 // Unused Flag
 #define FLAG_PETALBURG_WOODS_VISIITED    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_STARTOFGAME    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
